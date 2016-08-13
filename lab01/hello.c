3d165f5de765e2ac31c9f0662e48839db6929e87
hemantkrmmt <hemantkumar9904@gmail.com>
2016-08-13 12:55:00 +0530

    add screenshot

--- lab01/hello.c
+++ lab01/hello.c
@@ -1,52 +1,44 @@
-/****************************************************************************
-**
+/******************************************************************************
 * FILE: hello.c
 * DESCRIPTION:
-*
-A "hello world" Pthreads program. Demonstrates thread creation and
-*
-termination.
+*   A "hello world" Pthreads program.  Demonstrates thread creation and
+*   termination.
 * AUTHOR: Blaise Barney
 * LAST REVISED: 08/09/11
-*****************************************************************************
-*/
+******************************************************************************/
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define NUM_THREADS 5
-void *PrintHello(void *threadid){
-long tid;
-tid = (long)threadid;
-if (tid == 0) {
-  sleep(2);
+#include <unistd.h>
+#define NUM_THREADS	5
+
+void *PrintHello(void *threadid)
+{
+   long tid;
+   tid = (long)threadid;
+if(tid == 0){sleep(10);}
+if(tid == 1){sleep(50);}
+if(tid == 2){sleep(.20);}
+if(tid == 3){sleep(12);}
+   printf("Hello World! It's me, thread #%ld!\n", tid);
+
+   pthread_exit(NULL);
 }
-if (tid == 1) {
-  sleep(10);
-}
-if (tid == 2) {
-  sleep(.2);
-}
-if (tid == 3) {
-  sleep(.6);
-}
-if (tid == 4) {
-  sleep(.4);
-}
-printf("Hello World! It's me, thread #%ld!\n", tid);
-pthread_exit(NULL);
-}
-int main(int argc, char *argv[]){
-  pthread_t threads[NUM_THREADS];
-  int rc;
-  long t;
-  for(t=0;t<NUM_THREADS;t++){
-    printf("In main: creating thread %ld\n", t);
-    rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
-    if (rc){
-      printf("ERROR; return code from pthread_create() is %d\n", rc);
-      exit(-1);
-    }
-  }
-/* Last thing that main() should do */
-  pthread_exit(NULL);
+
+int main(int argc, char *argv[])
+{
+   pthread_t threads[NUM_THREADS];
+   int rc;
+   long t;
+   for(t=0;t<NUM_THREADS;t++){
+     printf("In main: creating thread %ld\n", t);
+     rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
+     if (rc){
+       printf("ERROR; return code from pthread_create() is %d\n", rc);
+       exit(-1);
+       }
+     }
+
+   /* Last thing that main() should do */
+   pthread_exit(NULL);
 }
